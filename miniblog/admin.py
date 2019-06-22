051b33191f27f98bdd1ffd31d5c7b5095b35344d
Michael <phamm89@gmail.com>
2019-06-21 21:48:49 -0400

    Registering Admin Classes Using Decorators

--- miniblog/admin.py
+++ miniblog/admin.py
@@ -3,7 +3,23 @@ from miniblog.models import Blog, BlogInstance, Blogger, BlogComment
 
 # Register models
 
-admin.site.register(Blog)
-admin.site.register(BlogInstance)
-admin.site.register(Blogger)
+# admin.site.register(Blog)
+# Register the Admin classes for Blog using the decorator
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    pass
+
+# admin.site.register(BlogInstance)
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BlogInstance) 
+class BlogInstanceAdmin(admin.ModelAdmin):
+    pass
+
+# Define the admin class
+# admin.site.register(Blogger)
+class BloggerAdmin(admin.ModelAdmin):
+    pass
+# Register the admin class with the associated model
+admin.site.register(Blogger, BloggerAdmin)
+
 admin.site.register(BlogComment)
